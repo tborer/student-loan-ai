e2a46032f218774285c7c65ef2b26755b6ae866e
Travis <travis.borer@outlook.com>
2026-09-19 11:10:52 -0500

    feat: Create App root component that renders LandingPage

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { LandingPage } from './components/LandingPage';
+
+const App: React.FC = () => {
+  return (
+    <div className="app">
+      <LandingPage />
+    </div>
+  );
+};
+
+export default App;
